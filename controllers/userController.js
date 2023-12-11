b88c6b7eb7c23e59dab18e5766ef446b0d354cbb
yJoelhenry7 <20pa1a5458@vishnu.edu.in>
2023-12-11 21:17:06 +0530

    Adding controller and routes for user register

---
+++ controllers/userController.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-vars */
+const { User, Sequelize } = require('../models')
+const user = {}
+
+// register a new user in user table
+user.register = async (req, res) => {
+  if (!req.body.username || !req.body.password || !req.body.userType) {
+    return res.status(400).send({
+      success: false,
+      message: 'Content can not be Empty!'
+    })
+  }
+  try {
+    const newUser = {
+      username: req.body.username,
+      password: req.body.password,
+      userType: req.body.userType
+    }
+    const newUserData = await User.create(newUser)
+    return res.status(201).json({
+      success: true,
+      data: newUserData
+    })
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error
+    })
+  }
+}
+
+module.exports = user
--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT | 8080
 
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+// ---------------------MiddleWares----------------------------------------------------
+// parse requests of content-type - application/json
+app.use(express.json())
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }))
+
+// -----------------------Routes---------------------------------------------------------
+const userRoute = require('./routes/userRoute')
+
+app.use('/api/auth/register', userRoute)
 
 app.listen(PORT, () => {
   console.log(`Listening at port ${PORT}`)
