d2ecf3ad39a0a12250ce3f72714782a13a7d5a5a
yJoelhenry7 <20pa1a5458@vishnu.edu.in>
2023-12-13 18:34:20 +0530

    adding code for create catalog

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,3 +1,4 @@
+/* eslint-disable dot-notation */
 /* eslint-disable no-unused-vars */
 const { User, Seller, Product, Op, Sequelize } = require('../models')
 const product = {}
@@ -6,10 +7,33 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 product.createCatalog = async (req, res) => {
-  const verifyToken = jwt.verify(req.body.token, process.env.SECRET)
+  if (!req.body.token || !req.body.productList) {
+    return res.status(400).send({
+      success: false,
+      message: 'Content can not be Empty!'
+    })
+  }
+  const verifyToken = jwt.verify(req.body.token, process.env.SECRET) // getting payload data using jwt token
+  const sellerId = verifyToken.sellerId // getting sellerId from payload
   const productList = req.body.productList
+  // getting sellerData using sellerId
+  const sellerData = await Seller.findOne({
+    where: {
+      id: sellerId
+    }
+  })
+  const catalogId = sellerData.catalogId
+  productList.forEach(async (product) => {
+    const productData = {
+      productName: product['productName'],
+      productPrize: product['price'],
+      catalogId
+    }
+    await Product.create(productData)
+  })
   if (verifyToken.userType === 'seller') {
     await res.status(201).json({
+      catalogId,
       productList
     })
   }
